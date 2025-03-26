1bfa76b997876a13e8d693eb71f83b666ec96587
Pedro Soares <143148971+pedrosoaresdev@users.noreply.github.com>
2025-03-26 19:10:46 -0300

    Add files via upload

---
+++ desafio.backend-btg-pactual/src/main/java/tech/pedro/btgpactual/orderms/controller/dto/OrderResponse.java
@@ -0,0 +1,14 @@
+package pedro.btgpactual.orderms.controller.dto;
+
+import pedro.btgpactual.orderms.entity.OrderEntity;
+
+import java.math.BigDecimal;
+
+public record OrderResponse(Long orderId,
+                            Long customerId,
+                            BigDecimal total) {
+
+    public static OrderResponse fromEntity(OrderEntity entity) {
+        return new OrderResponse(entity.getOrderId(), entity.getCustomerId(), entity.getTotal());
+    }
+}
---
+++ desafio.backend-btg-pactual/src/main/java/tech/pedro/btgpactual/orderms/listener/OrderCreatedListener.java
@@ -0,0 +1,30 @@
+package pedro.btgpactual.orderms.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+import pedro.btgpactual.orderms.listener.dto.OrderCreatedEvent;
+import pedro.btgpactual.orderms.service.OrderService;
+
+import static pedro.btgpactual.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
+
+@Component
+public class OrderCreatedListener {
+
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    private final OrderService orderService;
+
+    public OrderCreatedListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @RabbitListener(queues = ORDER_CREATED_QUEUE)
+    public void listen(Message<OrderCreatedEvent> message){
+        logger.info("Message consumed: {}", message);
+
+        orderService.save(message.getPayload());
+    }
+}
